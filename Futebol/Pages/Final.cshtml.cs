615ec7b7ca36ee0b2532f6952e79cc5f445de97b
Cristiano Rodrigues <kriks.red@gmail.com>
2022-09-05 22:14:02 -0300

    Adicionar arquivos de projeto.

---
+++ Futebol/Pages/Final.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Futebol.Pages
+{
+    public class FinalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
